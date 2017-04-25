4f510130cf28226ee263de24ed58414fb549fa60
Thibault Hilaire <thibault.hilaire@lip6.fr>
2017-04-25 18:01:04 +0200

    add the TM1638 datasheet

---
+++ test/test_TM.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+
+from rpi-TM1638 import TMBoards
+
+# my GPIO settings (two TM1638 boards connected on GPIO19 and GPIO13 for DataIO and Clock; and on GPIO06 and GPIO26 for the STB)
+DIO = 19
+CLK = 13
+STB = 06, 26
+
+# instanciante my TMboards
+TM = TMBoards(DIO, CLK, STB, 0)
+
+# some LEDs manipulation
+TM.leds[0] = True       # turn on led 0 (1st led of the 1st board)
+TM.leds[12] = True      # turn on led 12 (5th led of the 2nd board, since there is 8 leds per board)
+
+TM.segments[1] = '0'        # display '0' on the display 1 (2nd 7-segment display of the 1st board)
+TM.segments[4] = '98.76'     # display '9876' on the 7-segment display number 4, 5, 6 and 7 (the point is on segment 5)
+TM.segments[3,1] = True     # turn on the segment #1 of the 7-segment number 3
+
+TM.leds = (True, False, True)   # set the three first leds
